1aed525d3816c50aa2577484f7cdb8460a141236
Floweynt <50057682+Floweynt@users.noreply.github.com>
2025-09-24 16:19:11 -0500

    unbreak javadocs

--- coro/src/main/java/com/floweytf/coro/annotations/Coroutine.java
+++ coro/src/main/java/com/floweytf/coro/annotations/Coroutine.java
@@ -12,7 +12,7 @@ import java.lang.annotation.Target;
 
 /**
  * Annotation to mark a method as a coroutine. All coroutines are <i>stackless</i>.
- * <p>
+ * <br>
  * A coroutine is a method that may be suspended. All methods annotated with {@code Coroutine} must return a
  * {@link Task}. In a coroutine, methods such as {@link Co#await(Awaitable)} and {@link Co#ret()}/
  * {@link Co#ret(Object)} become available, allowing for imperative async code without callbacks. For example, an
@@ -24,13 +24,11 @@ import java.lang.annotation.Target;
  * }
  * return Co.ret();
  * }</pre>
- * </p>
- * <p>
+ * <br>
  * To return from a coroutine, you must use {@code return Co.ret([value]);}. Regular return statements are not accepted.
  * Void coroutines still must explicitly return once control flow reaches the end of the method. These limitations
  * are designed to ensure easy IDE integration without the use of any plugins.
- * </p>
- * <p>
+ * <br>
  * The execution of a coroutine is determined by the {@link CoroutineExecutor} that the coroutine is
  * started with in {@link Task#begin()}. The simplest executor eagerly executes the task on the same thread as the
  * completion. That is, if an {@link Awaitable} chooses to resume the coroutine on some arbitrary thread, the
@@ -51,7 +49,6 @@ import java.lang.annotation.Target;
  *     example().begin();
  * }
  * }</pre>
- * </p>
  *
  * @see Awaitable
  * @see CoroutineExecutor
